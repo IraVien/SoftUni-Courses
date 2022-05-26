e8212917e3d16bf5da591edb7a3bff5bbba651bb
IraVien <awakenhuman@abv.bg>
2022-05-26 18:54:02 +0300

    Added programming basics witch C# exercises review.

---
+++ SoftUni Programming Basics exercises review/T05.LoginEx2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace T05.LoginEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+            for (int i = username.Length-1; i >=0; i--)
+            {
+                password += username[i];
+            }
+            int counterPassTimes = 0;
+            string inputPassword = Console.ReadLine();
+            while (inputPassword!=password)
+            {
+                counterPassTimes++;
+                if (counterPassTimes>3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                inputPassword = Console.ReadLine();
+            }
+            if (inputPassword==password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+
+        }
+
+    }
+}
---
+++ SoftUni Programming Basics exercises review/T11.Orders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T11.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfOrders = int.Parse(Console.ReadLine());
+            double pricePerCapsule = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int capsulesCount = int.Parse(Console.ReadLine());
+
+            double priceForOrder = 0;
+            double sumPriceForOrder = 0;
+            int counterOrders = 0;
+            double totalPriceForOrders = 0;
+            while (counterOrders<numOfOrders)
+            {
+                counterOrders++;
+                if (counterOrders>1)
+                {
+                    pricePerCapsule = double.Parse(Console.ReadLine());
+                    days = int.Parse(Console.ReadLine());
+                    capsulesCount = int.Parse(Console.ReadLine());
+                }
+                priceForOrder = ((days * capsulesCount) * pricePerCapsule);
+                sumPriceForOrder += priceForOrder;
+                Console.WriteLine($"The price for the coffee is: ${sumPriceForOrder:f2}");
+                totalPriceForOrders += sumPriceForOrder;
+                  sumPriceForOrder= 0;
+                
+                
+            }
+            Console.WriteLine($"Total: ${totalPriceForOrders:f2}");
+        }
+    }
+}
